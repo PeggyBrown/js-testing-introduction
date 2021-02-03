0abfe57269371852575443eed823963083d4b86e
PeggyBrown <ola.qnysz@gmail.com>
2021-02-03 18:37:03 +0100

    Added e2e tests

--- util.test.js
+++ util.test.js
@@ -1,4 +1,3 @@
-
 const puppeteer = require("puppeteer");
 const {checkAndGenerate} = require("./util");
 const {generateText} = require('./util');
@@ -11,4 +10,22 @@ test('should output text name and age', () => {
 test('should generate a valid text output', () => {
     const text = checkAndGenerate('Mateusz', 24);
     expect(text).toBe('Mateusz (24 years old)');
+});
+
+test('should create correct element with text in the browser', async () => {
+    const browser = await puppeteer.launch({
+        headless: true,
+        // slowMo: 40,
+        // args: ["--windows-size=1920,1080"]
+    });
+
+    const page = await browser.newPage();
+    await page.goto('file:///Users/aleksandrakunysz/Documents/Szkoła Testów/js-testing-introduction/index.html');
+    await page.click("input#name");
+    await page.type("input#name", "Ania");
+    await page.click("input#age");
+    await page.type("input#age", "23");
+    await page.click("#btnAddUser");
+    const finalText = await page.$eval('.user-item', el => el.textContent);
+    expect(finalText).toBe("Ania (23 years old)");
 });
