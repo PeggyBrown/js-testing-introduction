8374ce9f43af49681a1268830ba3b7ff15b3e2bb
PeggyBrown <ola.qnysz@gmail.com>
2021-02-09 13:42:46 +0100

    Used date picker instead of number

--- util.js
+++ util.js
@@ -1,35 +1,40 @@
-const generateText = (name, age) => {
-  // Returns output text
-  return `${name} (${age} years old)`;
+const generateText = (name, birth) => {
+// Returns output text
+    return `${name} (${birth} years old)`;
 };
 
+exports.calculateAge = (birth) => {
+    let date = new Date(Date.parse(birth));
+    return Math.abs(2021 - date.getUTCFullYear());
+}
+
 exports.createElement = (type, text, className) => {
-  // Creates a new HTML element and returns it
-  const newElement = document.createElement(type);
-  newElement.classList.add(className);
-  newElement.textContent = text;
-  return newElement;
+    // Creates a new HTML element and returns it
+    const newElement = document.createElement(type);
+    newElement.classList.add(className);
+    newElement.textContent = text;
+    return newElement;
 };
 
 const validateInput = (text, notEmpty, isNumber) => {
-  // Validate user input with two pre-defined rules
-  if (!text) {
-    return false;
-  }
-  if (notEmpty && text.trim().length === 0) {
-    return false;
-  }
-  if (isNumber && +text === NaN) {
-    return false;
-  }
-  return true;
+    // Validate user input with two pre-defined rules
+    if (!text) {
+        return false;
+    }
+    if (notEmpty && text.trim().length === 0) {
+        return false;
+    }
+    if (isNumber && +text === NaN) {
+        return false;
+    }
+    return true;
 };
 
-exports.checkAndGenerate = (name, age) => {
-  if (!validateInput(name, true, false) || !validateInput(age, false, true)) {
-    return false;
-  }
-  return generateText(name, age);
+exports.checkAndGenerate = (name, birth) => {
+    if (!validateInput(name, true, false)) {
+        return false;
+    }
+    return generateText(name, birth);
 };
 
 exports.generateText = generateText;
